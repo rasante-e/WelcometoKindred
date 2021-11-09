e5f432d45df0cc8780072b772b7dba5d54a86667
Rashida <rashida.asante@gmail.com>
2021-11-09 00:21:30 -0500

    Add validation for number in form

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,6 +1,37 @@
   /* For form validation */
 
-  const email = getElementById("");
+
+  
+  
+
+  function validateForm () {
+    const years = document.getElementById("years-response").value;
+
+if (isNaN(years) || (years == "")) {
+ /* alert("Please enter a valid number"); */
+ document.getElementById("congrats").innerHTML="Please enter a valid number";
+}
+else {
+  document.getElementById("congrats").innerHTML="Thank you for being a part of our community for " + years +
+  " years! We will reach out regarding your initiation into our coven soon.";
+}
+/*   
+if (years != 10) {
+  alert("Hooray!");
+}
+
+else {
+  alert("Correct!");
+}
+*/
+
+/* alert(email); */
+  /*
+    const email = document.getElementById("years-response").value;
+    console.log(email);
+    */
+  }
+
 
   /* 1. Store Form entry as a variable */
 
